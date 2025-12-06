c209244683c2c9de11086cc91e4d2d9fe37dad16
Diogosousa03 <dmrllsousa@gmail.com>
2025-12-06 20:04:50 +0000

    rbca casbin integrated

---
+++ index.js
@@ -0,0 +1,17 @@
+const { newEnforcer } = require('casbin');
+
+const pdp = async function(s, o, a) {
+  const enforcer = await newEnforcer('casbin/model.conf', 'casbin/basic_policy.csv');
+  r = await enforcer.enforce(s, o, a);
+  return {res: r, sub: s, obj: o, act: a};
+}
+
+
+const pep = function(decision) {
+  console.log(decision);
+  if (decision.res == true) {
+    console.log("permit operation")
+  } else {
+    console.log("deny operation")
+  }  
+}
